55e1b6d5c2776469d9ba53c1ea692e71e72d30e3
Abhisek20 <69687280+Abhisek20@users.noreply.github.com>
2021-05-26 20:33:40 +0530

    Add files via upload

--- WebApp.py
+++ WebApp.py
@@ -22,22 +22,27 @@ pkl_scale = pickle.load(open('housing_scale.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('layout.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+    
+    # Loads the inputs from UI
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    #print('Response', final_features)
+
+    # Scaling the input features- MinMaxScaling
     final_features = pkl_scale.scale_[1:]*(final_features)[0]
+    
+    # Using the trained model, prediction is done
     prediction = pkl_model.predict([final_features])
+    
+    # Scaling the output back to input scale for interpretation
     prediction = prediction*1/pkl_scale.scale_[0]
+    
     output = str(round(prediction[0], 2))
 
-    return render_template('index.html', prediction_text='Housing price should be INR {}'.format(output))
+    return render_template('layout.html', prediction_text='Housing price should be INR {}'.format(output))
 
 
 if __name__ == "__main__":
